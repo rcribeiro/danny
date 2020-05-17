f899f18db37b3a6f377330edd6bdcd3e33c6c6af
Ricardo Ribeiro <rcribeiro@me.com>
2020-05-17 17:22:52 -0300

    Updates

---
+++ precache-manifest.b297ab57fb5a68f0ba012d5529b20eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e076c6508e2f05b4eb5",
+    "url": "/static/css/main.dd1ef2de.chunk.css"
+  },
+  {
+    "revision": "7e076c6508e2f05b4eb5",
+    "url": "/static/js/main.7e076c65.chunk.js"
+  },
+  {
+    "revision": "1836f49fc3d871cf44d1",
+    "url": "/static/css/1.17f22373.chunk.css"
+  },
+  {
+    "revision": "1836f49fc3d871cf44d1",
+    "url": "/static/js/1.1836f49f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f9abcd2726bd7fd085a05029078ad7d",
+    "url": "/index.html"
+  }
+];
